18d9fa2ffa7296f8b54508d37d2081965b98b88d
Anav Chand <anavkki@gmail.com>
2024-08-05 18:36:50 +1200

    feat: add pointers examples

--- Pointers.cpp
+++ Pointers.cpp
@@ -1,20 +1,53 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
+
+void doubleNumber(int *number);
 
 int main()
 {
+    cout << left;
 
-    int x = 5;
-    int *ptr = &x;
-    int &e = *ptr;
+    // ------- pointers dereferencing ------- //
 
-    cout << "x before: " << x << endl;
+    int number = 10;
+    int *pointerToNumber = &number;
 
-    *ptr = 10;
+    cout << "Address: " << pointerToNumber << endl;
+    cout << "Value: " << *pointerToNumber << endl;
 
-    cout << "x: " << x << endl;
-    cout << "pointer: " << e << endl;
+    cout << endl;
 
-    system("PAUSE");
+    //---------- pointers with array ----------//
+
+    int numbers[5] = {2, 4, 6, 8, 10};
+
+    for (int i = 0; i < 5; i++)
+    {
+        int *currentPtr = &numbers[i];
+
+        cout << setw(20) << "Current address: " << currentPtr << endl;
+        cout << setw(20) << "Current value: " << *currentPtr << endl;
+
+        // store address of next memory block
+
+        int *nextPtr = ++currentPtr;
+
+        cout << setw(20) << "Next address: " << nextPtr << endl;
+        cout << setw(20) << "Next value: " << *nextPtr << endl;
+    }
+
+    cout << endl;
+
+    //-------- pointers with functions --------//
+
+    doubleNumber(&number);
+    cout << "Doubled number: " << number << endl;
+
     return 0;
+}
+
+void doubleNumber(int *number)
+{
+    *number *= 2;
 }
