5241e8febcc2af8697b67be7691f31949bc9a216
Anav Chand <anavkki@gmail.com>
2024-10-03 13:33:12 +1200

    feat: add recursion programs

--- recursion/recursive-math.cpp
+++ recursion/recursive-math.cpp
@@ -31,10 +31,22 @@ int recursiveExponent(int base, int exponent) {
     }
 }
 
+int recursiveGCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+
+    else {
+        return recursiveGCD(b, a % b);
+    }
+} 
+
 int main() {   
     cout << "Sum: " << recursiveSum(10) << endl;
     cout << "Range: " << recursiveSum(10) - recursiveSum(5) << endl;
     cout << "Factorial: " << recursiveFactorial(5) << endl;
     cout << "Exponent: " << recursiveExponent(2, 5) << endl;
+    cout << "GCD of 125 and 100: " << recursiveGCD(125, 100) << endl;
+
     return 0;
 }
