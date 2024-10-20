ec9a353423b25b43f08c298341323dda25bdf38a
Anav Chand <anavkki@gmail.com>
2024-10-20 22:48:31 +1200

    feat: add doubly linked list program

--- linked-lists/doubly-linked-list.cpp
+++ linked-lists/doubly-linked-list.cpp
@@ -1,26 +1,109 @@
 #include <iostream>
 using namespace std;
 
-struct Node
+class Node
 {
+public:
+    Node *prev;
+    Node *next;
     int data;
-    Node *next = NULL;
-    Node *prev = NULL;
+
+    Node(int data)
+    {
+        prev = NULL;
+        next = NULL;
+        this->data = data;
+    }
+};
+
+class List
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    ~List()
+    {
+        while (head != NULL)
+        {
+            cout << "Removed node with data: " << head->data << endl;
+            removeNode(head);
+        }
+    }
+
+    void appendNode(Node *newNode)
+    {
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+        }
+
+        tail = newNode;
+
+        cout << "Appended node with data: " << newNode->data << endl;
+    }
+
+    void removeNode(Node *removeNode)
+    {
+        // Check if node to remove is head
+        if (removeNode->prev == NULL)
+        {
+
+            head = removeNode->next;
+        }
+
+        else
+        {
+
+            removeNode->prev->next = removeNode->next;
+        }
+
+        // Check is node to remove is tail
+        if (removeNode->next == NULL)
+        {
+
+            tail = removeNode->prev;
+        }
+
+        else
+        {
+
+            removeNode->next->prev = removeNode->prev;
+        }
+
+        delete removeNode;
+    }
+
+    Node *getNode(int index)
+    {
+        Node *node = head;
+
+        for (int i = 0; i < index; i++)
+        {
+            node = node->next;
+        }
+
+        return node;
+    }
 };
 
 int main()
 {
-    Node *linkedListPtr = new Node;
+    List *list = new List;
 
-    linkedListPtr->next = new Node;
+    list->appendNode(new Node(1));
+    list->appendNode(new Node(2));
+    list->appendNode(new Node(3));
+    list->appendNode(new Node(4));
+    list->appendNode(new Node(5));
 
-    linkedListPtr->next->data = 5;
-
-    linkedListPtr->next->next = new Node;
-
-    linkedListPtr->next->next->data = 10;
-
-    linkedListPtr->next->next->prev = linkedListPtr->next;
+    delete list;
 
     return 0;
 }
